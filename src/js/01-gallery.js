2c6528837700a11d8ab7dc3e1f5dc5d8c98c55d7
Strekozak <ekateryna.p@gmail.com>
2023-08-08 00:44:39 +0300

    2 task f

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryUl = document.querySelector(".gallery");
+
+function gallaryCreate() {
+    const galleryList = galleryItems.map(item =>
+        `<li class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+        <img class="gallery__image"
+        src="${item.preview}"
+        alt="${item.description}" 
+        target="_parent"/>
+        </a>
+        </li>`)
+        .join("");
+    
+    galleryUl.innerHTML = galleryList;
+};
+
+gallaryCreate();
+
+let galleryBox = new SimpleLightbox('.gallery a', {
+    captions: "true",
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+            
+});
